bcff48b5880fe5794031d151bbda702a284978ae
Zander Keith <zkeith@psfc.mit.edu>
2023-06-01 17:35:37 -0400

    Improved plotting, separated load_dataset into two separate functions

--- plot_utils.py
+++ plot_utils.py
@@ -1,7 +1,7 @@
 from preprocess_datasets import load_dataset
 import matplotlib.pyplot as plt
 
-def plot_shot(device, dataset, shot_number):
+def plot_shot(device, dataset, shot_number, match_times=True):
     """ Plots the time series data for a given shot in a dataset
     Parameters
     ----------
@@ -13,16 +13,31 @@ def plot_shot(device, dataset, shot_number):
         The shot number to plot
     """
 
-    notused, notused, data = load_dataset(device, dataset)
+    data = load_dataset(device=device, dataset=dataset)
 
     shot = data[data['shot'] == shot_number]
     shot = shot.sort_values('time')
 
+    # Determine if the shot was disrupted
+    disrupted = shot['time_until_disrupt'].notnull().any()
+
     # Put each feature on a separate subplot
-    fig, axes = plt.subplots(len(shot.columns) - 2, 1, sharex=True, figsize=(10, 10))
-    for i, col in enumerate(shot.columns[2:]):
-        axes[i].plot(shot['time'], shot[col])
+    fig, axes = plt.subplots(len(shot.columns) - 4, 1, sharex=True, figsize=(10, 10))
+    for i, col in enumerate(shot.columns[4:]):
+        axes[i].plot(shot['time'], shot[col], color=plt.rcParams['axes.prop_cycle'].by_key()['color'][i])
         axes[i].set_ylabel(col)
+        if match_times:
+            axes[i].set_xlim(data['time'].min(), data['time'].max())
     axes[-1].set_xlabel('time')
+
+    # Add a title
+    caption = '{} shot {} from {} dataset'.format(device, shot_number, dataset)
+    if disrupted:
+        caption += '\n(disrupted)'
+    else:
+        caption += ('\n(not disrupted)')
+    
+    fig.suptitle(caption)
+
     fig.show()
 
--- preprocess_datasets.py
+++ preprocess_datasets.py
@@ -6,17 +6,45 @@ import pandas as pd
 
 def load_dataset(device='cmod', dataset='small_set'):
     """ Load the dataset from the given device and dataset
+    Parameters
+    ----------
+    device : str
+        The device for which to load the data
+    dataset : str
+        The dataset to load
+    Returns
+    -------
+    data : pandas.DataFrame
+        A dataframe containing the data in the dataset
+        In the form of unsorted timeslices of all included shots
     """
-    
     data = pkgutil.get_data(__name__, 'data/{}/{}.csv'.format(device, dataset))
     data = pd.read_csv(io.BytesIO(data))
+    return data
+
+
+def load_dataset_train(device='cmod', dataset='small_set'):
+    """ Load the specified dataset from a device,
+    and return the features and outcomes for training
+    Parameters
+    ----------
+    device : str
+        The device for which to load the data
+    dataset : str
+        The dataset to load
+    Returns
+    -------
+    outcomes : pandas.DataFrame
+        A dataframe containing the outcomes for each shot
+        Outcomes are whether or not the event occurred, and the time until the event or last measurement
+    features : pandas.DataFrame
+        A dataframe containing the features for each shot
+        TODO: should this match DPRF ordering?
+    """
+    data = load_dataset(device=device, dataset=dataset)
 
     # Add time of last measurement for each shot
     data['last_time'] = data.groupby('shot')['time'].transform('max')
-
-    # Remove the last measurement for each shot
-    # TODO: this is a hack to stop negative times from showing up, make better
-    data = data[data['time'] != data['last_time']]
 
     # For each timeslice, determine if the shot was disrupted
     # And the time until disruption or last measurement
@@ -32,6 +60,5 @@ def load_dataset(device='cmod', dataset='small_set'):
     # TODO: add in aminor, squareness, and triangularity 
     train_feats = ['ip','Wmhd','n_e','kappa','li']
 
-    # Also return a dataset with features for plotting
+    return outcomes, data[train_feats]
 
-    return outcomes, data[train_feats], data[['shot', 'time'] + train_feats]
