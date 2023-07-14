d85bb47b913921b4a15f4e6d82cd2300de2953a5
Zander <zkeith@mit.edu>
2023-07-14 15:19:00 -0400

    Changed plot x-axes to ms

--- plot_experiments.py
+++ plot_experiments.py
@@ -8,23 +8,27 @@ from Experiments import Experiment
 
 # Plots for 
 
-def plot_roc_auc_vs_horizon_macro(experiment_list:list[Experiment], horizons=np.linspace(0.01, 0.4, 10)):
+def plot_roc_auc_vs_horizon_macro(experiment_list:list[Experiment], horizons=np.linspace(0.01, 0.4, 9)):
     """ Averaged over all shots
     
     """
+
+    horizons_ms = horizons*1000
 
     plt.figure()
     
     for experiment in experiment_list:
         roc_auc, roc_auc_std = experiment.roc_auc_macro(horizons)
-        plt.errorbar(horizons, roc_auc, yerr=roc_auc_std, label=experiment.name, fmt='o-')
+        plt.errorbar(horizons_ms, roc_auc, yerr=roc_auc_std, label=experiment.name, fmt='o-')
         #plt.plot(horizons, roc_auc, label=experiment.name)
         #plt.fill_between(horizons, roc_auc - roc_auc_std, roc_auc + roc_auc_std, alpha=0.2)
 
-    plt.xlim([horizons[0], horizons[-1]])
+    plt.xlim([horizons_ms[0], horizons_ms[-1]])
     plt.ylim([0.5, 1])
 
-    plt.xlabel('Horizon [s]')
+    plt.xticks(horizons_ms)
+
+    plt.xlabel('Horizon [ms]')
     plt.ylabel('Macro Average Area Under ROC Curve')
 
     plt.title('Macro Average ROC AUC vs. Horizon')
@@ -38,16 +42,20 @@ def plot_roc_auc_vs_horizon_micro(experiment_list:list[Experiment], horizons=np.
 
     """
 
+    horizons_ms = horizons*1000
+
     plt.figure()
 
     for experiment in experiment_list:
         roc_auc = experiment.roc_auc_micro_all(horizons)
-        plt.plot(horizons, roc_auc, label=experiment.name)
+        plt.plot(horizons_ms, roc_auc, label=experiment.name)
 
-    plt.xlim([horizons[0], horizons[-1]])
+    plt.xlim([horizons_ms[0], horizons_ms[-1]])
     plt.ylim([0.5, 1])
 
-    plt.xlabel('Horizon [s]')
+    plt.xticks(horizons_ms)
+
+    plt.xlabel('Horizon [ms]')
     plt.ylabel('Micro Average Area Under ROC Curve')
 
     plt.title('Micro Average ROC AUC vs. Horizon')
