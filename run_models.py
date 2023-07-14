b68292a094bc8a1923e59d9103a2a67ae0566938
Zander <zkeith@mit.edu>
2023-07-14 18:08:05 -0400

    Added method for creating 'stacks' of features

--- run_models.py
+++ run_models.py
@@ -30,14 +30,12 @@ def get_train_times(y_tr):
     except:
         return np.quantile(y_tr['time'], np.linspace(0.1, 0.9, 10)).tolist()
 
-
 def get_test_times(y_tr):
     """
     Get the test times for survival models
     """
     # TODO: this should be limited to under 500 ms
     return np.quantile(y_tr['time'][y_tr['event']==1], np.linspace(0.1, 0.9, 10)).tolist()
-
 
 def run_survival_model(model_string, x_tr, x_val, y_tr, y_val, selection='rough'):
     """
