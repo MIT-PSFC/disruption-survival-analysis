711512da2237738c726f7cc75a1abdc58d187c81
Zander <zkeith@mit.edu>
2023-07-19 20:36:04 -0400

    Refactored to separate risk calculation logic from warning time logic

--- experiment_utils.py
+++ experiment_utils.py
@@ -31,3 +31,67 @@ def label_shot_data(shot_data, disrupt, horizon):
         labeled_data = np.zeros(len(shot_data))
 
     return labeled_data
+
+def calculate_disruption_time(risk_at_time, thresholds):
+    """
+    Calculates the disruption time(s) for a given shot with a simple threshold
+
+    Parameters
+    ----------
+    risk_at_time : pandas.DataFrame
+        The risk of disruption for each time slice in a single shot
+        Should be sorted by time
+        Should be transformed by the predictor's transformer
+    thresholds : list of float
+        The thresholds to use for determining if a disruption is imminent
+
+    Returns
+    -------
+    disruption_times : list of float
+        The times of disruption
+        If no disruption is predicted, returns None in that position
+    """
+    
+    # Make a copy of the thresholds it to keep track of which have been used
+    if isinstance(thresholds, np.ndarray):
+        avail_thresholds = thresholds.tolist()
+    else:
+        avail_thresholds = thresholds.copy()
+
+    disruption_times = []
+    # Go through the shot data and find the first time the risk exceeds each threshold
+    for i in range(len(risk_at_time)):
+        # If there are no more thresholds, stop
+        if len(avail_thresholds) == 0:
+            break
+
+        # If the risk ever exceeds the threshold, add the time to the list
+        # and remove the threshold from the list
+        # Then keep going until the risk is below the next threshold
+        # or there are no more thresholds
+        risk = risk_at_time.iloc[i]['risk']
+        while risk > avail_thresholds[0]:
+            disruption_times.append(risk_at_time.iloc[i]['time'])
+            avail_thresholds.pop(0)
+            if len(avail_thresholds) == 0:
+                break
+    
+    # If there is a mismatch between disruption times and thresholds,
+    # fill in the rest of the disruption times with None
+    if len(disruption_times) < len(thresholds):
+        for i in range(len(thresholds) - len(disruption_times)):
+            disruption_times.append(None)
+
+    # Return the disruption times
+    return disruption_times
+
+def calculate_disruption_time_hysterisis(self, shot_data, 
+                                            upper_threshold, lower_threshold, 
+                                            window, horizon):
+    """
+    Calculates the disruption time(s) for a given shot with hysterisis method
+    If the 'disruptivity' output of the model goes above the upper threshold
+    and remains above the lower threshold for the window length, a disruption
+    is predicted
+    """
+    raise NotImplementedError("Hysterisis method not yet implemented")
