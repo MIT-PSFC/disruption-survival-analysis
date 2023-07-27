262e73ea34c03934ec89fcfb0a59f04e595b7606
Zander <zkeith@mit.edu>
2023-07-27 19:21:15 -0400

    Have sweep creation set up, have job set up, and evaluation set up, but for some reason when running job from command line it breaks

--- wandb_job.py
+++ wandb_job.py
@@ -7,17 +7,8 @@ SURVIVAL_MODELS = ['cph', 'dcph', 'dcm', 'dsm', 'rsf']
 def run_wandb():
     run = wandb.init()
 
-    # Get the sweep name
-    sweep_name = wandb.config.name
-
-    # Split the sweep name by the '-' character
-    split_name = sweep_name.split('-')
-    device = split_name[0]
-    dataset_path = split_name[1]
-    model_type = split_name[2]
-    evaluation_method = split_name[3]
-
     # Positions to validate the model on
+    # TODO: I want to get rid of this, just trying it out for now
     valmin = wandb.config.valmin
     valmax = wandb.config.valmax
     numval = wandb.config.numval
@@ -32,15 +23,29 @@ def run_wandb():
             # Create model with parameters
             model = SurvivalModel('cph', l2=l2)
 
-        elif model_type == 'dcph':
-            from auton_survival.estimators import SurvivalModel # CPH, DCPH, DSM, DCM, RSF
-
+        else:
+            model = None
 
     metric_val = evaluate_model(device, dataset_path, model, evaluation_method, valmin, valmax, numval)
 
     wandb.log({evaluation_method: metric_val})
 
 def main():
+    # This is a bit of a hack because I don't want to pass variables to this special 'wandb function'
     sweep_id = sys.argv[1]
+    
+    global device 
+    device = sys.argv[2]
+
+    global dataset_path
+    dataset_path = sys.argv[3]
+
+    global model_type
+    model_type = sys.argv[4]
+
+    global evaluation_method 
+    evaluation_method = sys.argv[5]
+
     wandb.agent(sweep_id, function=run_wandb)
 
+main()
