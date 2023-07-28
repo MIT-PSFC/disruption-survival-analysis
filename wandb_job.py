c833735922ab205fc4eb43fe2607635065c13853
Zander <zkeith@mit.edu>
2023-07-27 20:47:24 -0400

    Trying this on Linux, seeing if that fixes the pathing issues

--- wandb_job.py
+++ wandb_job.py
@@ -1,4 +1,4 @@
-import sys
+import os
 import wandb
 from model_evaluation import evaluate_model
 
@@ -10,30 +10,51 @@ dataset_path = 'random_flattop_256_shots_60%_disruptive'
 model_type = 'cph'
 evaluation_method = 'timeslice_ibs'
 
-def run_wandb():
-    run = wandb.init(project='local-sweep')
+canary_file = 'wandb_job.py'
 
-    # Positions to validate the model on
-    # TODO: I want to get rid of this, just trying it out for now
-    valmin = wandb.config.valmin
-    valmax = wandb.config.valmax
-    numval = wandb.config.numval
+print("Running Job")
 
-    if model_type in SURVIVAL_MODELS:
-        from auton_survival.estimators import SurvivalModel
-        
-        if model_type == 'cph':
-            # Parameters for this type of model
-            l2 = wandb.config.l2
+def fix_pathing():
+    print("Current working directory:")
+    print(os.getcwd())
 
-            # Create model with parameters
-            model = SurvivalModel('cph', l2=l2)
+    # Check if can see a file in the project directory
+    print("Checking for file in project directory:")
+    if os.path.isfile(canary_file):
+        print("Found file")
+    else:
+        print("Did not find file")
+        print("Attempting directory change")
+        os.chdir(os.getenv('HOME'))
+        os.chdir("Documents")
+        os.chdir("Risk-Aware Frameworks")
+        os.chdir("disruption-survival-analysis")
+        if(os.path.isfile(canary_file)):
+            print("Found file")
 
-        else:
-            model = None
+fix_pathing()
 
-    metric_val = evaluate_model(device, dataset_path, model, evaluation_method, valmin, valmax, numval)
+run = wandb.init(project='local-sweep')
 
-    wandb.log({evaluation_method: metric_val})
+# Positions to validate the model on
+# TODO: I want to get rid of this, just trying it out for now
+valmin = wandb.config.valmin
+valmax = wandb.config.valmax
+numval = wandb.config.numval
 
-run_wandb()
+if model_type in SURVIVAL_MODELS:
+    from auton_survival.estimators import SurvivalModel
+    
+    if model_type == 'cph':
+        # Parameters for this type of model
+        l2 = wandb.config.l2
+
+        # Create model with parameters
+        model = SurvivalModel('cph', l2=l2)
+
+    else:
+        model = None
+
+metric_val = evaluate_model(device, dataset_path, model, evaluation_method, valmin, valmax, numval)
+
+wandb.log({evaluation_method: metric_val})
