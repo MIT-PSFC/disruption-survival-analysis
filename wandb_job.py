aac1946769b37fa1e3528a49932831aeb4045b93
ZanderKeith <zkeith@mit.edu>
2023-08-02 09:34:37 -0400

    Did some big DSM sweeps, seems to be working fine

--- wandb_job.py
+++ wandb_job.py
@@ -6,8 +6,9 @@ SURVIVAL_MODELS = ['cph', 'dcph', 'dcm', 'dsm', 'rsf']
 
 # This is a bit of a hack because I don't want to pass variables to this special 'wandb function'
 device = 'cmod'
-dataset_path = 'random_flattop_256_shots_60%_disruptive'
-model_type = 'cph'
+#dataset_path = 'random_flattop_256_shots_60%_disruptive'
+dataset_path = 'no_ufo_flattop_1452_shots_50%_disruptive'
+model_type = 'dsm'
 evaluation_method = 'timeslice_ibs'
 
 canary_file = 'wandb_job.py'
@@ -22,15 +23,6 @@ def fix_pathing():
     print("Checking for file in project directory:")
     if os.path.isfile(canary_file):
         print("Found file")
-    else:
-        print("Did not find file")
-        print("Attempting directory change")
-        os.chdir(os.getenv('HOME'))
-        os.chdir("Documents")
-        os.chdir("Risk-Aware Frameworks")
-        os.chdir("disruption-survival-analysis")
-        if(os.path.isfile(canary_file)):
-            print("Found file")
 
 fix_pathing()
 
@@ -52,8 +44,20 @@ if model_type in SURVIVAL_MODELS:
         # Create model with parameters
         model = SurvivalModel('cph', l2=l2)
 
-    else:
-        model = None
+    elif model_type == 'dsm':
+        # Parameters for this type of model
+        layers_str = wandb.config.layers
+        layers = [int(i) for i in layers_str.split('_')]
+        distribution = wandb.config.distribution
+        temperature = wandb.config.temperature
+        batch_size = wandb.config.batch_size
+        learning_rate = wandb.config.learning_rate
+        epochs = wandb.config.epochs
+        max_features = wandb.config.max_features
+        k = wandb.config.k
+
+        # Create model with parameters
+        model = SurvivalModel('dsm', layers=layers, distribution=distribution, temperature=temperature, batch_size=batch_size, learning_rate=learning_rate, epochs=epochs, max_features=max_features, k=k)
 
 metric_val = evaluate_model(device, dataset_path, model, evaluation_method, valmin, valmax, numval)
 
