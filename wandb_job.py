f7bd3802b72242544a798f9513664b57bf454ef0
Zander <zkeith@mit.edu>
2023-08-02 14:58:18 -0400

    Ensuring can get self-made fields out of wandb.config

--- wandb_job.py
+++ wandb_job.py
@@ -1,15 +1,9 @@
 import os
 import wandb
+from model_utils import make_survival_model
 from model_evaluation import evaluate_model
 
 SURVIVAL_MODELS = ['cph', 'dcph', 'dcm', 'dsm', 'rsf']
-
-# This is a bit of a hack because I don't want to pass variables to this special 'wandb function'
-device = 'cmod'
-#dataset_path = 'random_flattop_256_shots_60%_disruptive'
-dataset_path = 'no_ufo_flattop_1452_shots_50%_disruptive'
-model_type = 'dsm'
-evaluation_method = 'timeslice_ibs'
 
 canary_file = 'wandb_job.py'
 
@@ -36,8 +30,9 @@ print("==================")
 print("======ENDFIG======")
 print("==================")
 
-model = make_model(wandb.config)
+if wandb.config["model_type"] in SURVIVAL_MODELS:
+    model = make_survival_model(wandb.config)
 
-metric_val = evaluate_model(model, wandb.config)
+#metric_val = evaluate_model(model, wandb.config)
 
-wandb.log({evaluation_method: metric_val})
+#wandb.log({wandb.metri: metric_val})
