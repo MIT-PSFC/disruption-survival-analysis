76a9c05def9d645d51c5b6c7b3d87d7d75220603
zkeith <zkeith@eofe4.mit.edu>
2023-08-02 15:14:18 -0400

    Refactored to put the dataset-specific information in the hyperparameters

--- wandb_job.py
+++ wandb_job.py
@@ -5,23 +5,9 @@ from model_evaluation import evaluate_model
 
 SURVIVAL_MODELS = ['cph', 'dcph', 'dcm', 'dsm', 'rsf']
 
-canary_file = 'wandb_job.py'
+run = wandb.init()
 
-print("Running Job")
-
-def fix_pathing():
-    print("Current working directory:")
-    print(os.getcwd())
-
-    # Check if can see a file in the project directory
-    print("Checking for file in project directory:")
-    if os.path.isfile(canary_file):
-        print("Found file")
-
-fix_pathing()
-
-#run = wandb.init(project='local-sweep')
-
+"""
 print("==================")
 print("======CONFIG======")
 print("==================")
@@ -29,6 +15,7 @@ print(wandb.config)
 print("==================")
 print("======ENDFIG======")
 print("==================")
+"""
 
 if wandb.config["model_type"] in SURVIVAL_MODELS:
     model = make_survival_model(wandb.config)
