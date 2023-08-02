d4c45185596b5fd3a61d32ffe4e5900dd76cf40b
zkeith <zkeith@eofe4.mit.edu>
2023-08-02 18:40:58 -0400

    Need to read more about SLURM before running

--- job_instance.sh
+++ job_instance.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
+#SBATCH --job-name=cph_test_sweep
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -p sched_mit_psfc_gpu_r8
+#SBATCH -t 0-01:00
+#SBATCH --array
 
-source "/c/users/zkeith/documents/Risk-Aware Frameworks/disruption-survival-analysis/"
+source "etc/profile"
 
 # Load modules (if necessary)
 
 
 # Activate Python environment
-source "/c/users/zkeith/documents/Risk-Aware Frameworks/disruption-survival-analysis/.venv/Scripts/activate"
-which python
+source "~/projects/disruption-survival-analysis/.venv/Scripts/activate"
 
 # Run instantiation of WandB agent with given url
 wandb agent "$1"
