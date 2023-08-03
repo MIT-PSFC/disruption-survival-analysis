d8ffa98267d833aee6a40290399177d62de09ddf
zkeith <zkeith@eofe4.mit.edu>
2023-08-03 14:45:01 -0400

    Fixed SLURM submission scripts to actually run tasks in parallel

--- job_launch.sh
+++ job_launch.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 #Input arguments are <number of times for script to run> <url of wandb run>
-for i in {1.."$1"}
-do
-    sbatch job_instance.slurm "$2"
-done
+sbatch --array=0-$1 job_instance.slurm "$2"
