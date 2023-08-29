0371921bc6dda8fe63613d7a21d314da178e2dc9
ZanderKeith <zkeith@mit.edu>
2023-08-29 15:48:04 -0400

    Made bash script to allow for easily launching all sweep agents

--- job_launch_all.sh
+++ job_launch_all.sh
@@ -2,7 +2,13 @@
 #Launch one wandb agent for each sweep config file in the directory
 project_name="test-multi-sweep"
 
-directory=$1
+# Activate Python environment
+source /etc/profile
+source ~/Documents/disruption-survival-analysis/.venv/bin/activate
+
+#wandb --version
+
+directory="models/synthetic/synthetic100"
 
 # Get names of all sweep config files in directory
 files=$(ls $directory | grep "sweep")
@@ -10,14 +16,23 @@ files=$(ls $directory | grep "sweep")
 # Make a wandb agent for each sweep config file
 for file in $files
 do
+    echo "Launching agent for $file"
     # Launch the wandb agent
-    wandb sweep --project $project_name $directory/$file
-    # Get the sweep id from the output of the wandb sweep command
-    sweep_id=$(echo $output | grep -oP '(?<=sweep\/).*(?=\/)')
+    # Launching the sweep produces 4 lines of text, the last of which contains the sweep url
+    # We want to obtain this url
+    # The url appears after the string "wandb: Run sweep agent with: "
+    sweep_launch_output="$(wandb sweep --project $project_name $directory/$file 2>&1)"
+    last_line=$(echo "$sweep_launch_output" | tail -n 1)
+    echo "$last_line"
+    sweep_url=$(echo $last_line | sed 's/wandb: Run sweep agent with: wandb agent //g')
 
-    # Change the job name in the job_instance.slurm file
-    sed -i "s/NAME/$sweep_id/g" job_instance.slurm
+    echo "Sweep url: $sweep_url"
+
+    # Change the --job-name SBATCH argument in the job_instance.slurm file
+    # to the name of the sweep config, minus .yaml
+    sweep_id=$(echo $file | sed 's/.yaml//g')
+    sed -i "s/--job-name=.*/--job-name=$sweep_id/g" job_instance.slurm
 
     # Launch the job
-    sbatch --array=0-0 job_instance.slurm "$sweep_id"
+    ./job_launch 0 $sweep_url
 done
