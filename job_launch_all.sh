1cca03cf1a55bfb589ccf9f4462f192f4893437f
Zander <zkeith@mit.edu>
2023-09-26 20:26:47 -0400

    Refactored batch job launching for optuna

--- job_launch_all.sh
+++ job_launch_all.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-#Launch one wandb agent for each sweep config file in the directory
-project_name="test-multi-sweep-2"
+#Launch 8 hyperparameter tuning workers for each sweep config file in the directory
+project_name="test-multi-sweep-optuna"
 
 # Activate Python environment
 source /etc/profile
 source ~/projects/disruption-survival-analysis/.venv/bin/activate
 
-#wandb --version
-
-directory="models/cmod/no_ufo_flattop_7736_shots_6%_disruptive"
+directory="models/synthetic/test"
 
 # Get names of all sweep config files in directory
 files=$(ls $directory | grep "sweep")
@@ -17,16 +15,6 @@ files=$(ls $directory | grep "sweep")
 for file in $files
 do
     echo "Launching agent for $file"
-    # Launch the wandb agent
-    # Launching the sweep produces 4 lines of text, the last of which contains the sweep url
-    # We want to obtain this url
-    # The url appears after the string "wandb: Run sweep agent with: "
-    sweep_launch_output="$(wandb sweep --project $project_name $directory/$file 2>&1)"
-    last_line=$(echo "$sweep_launch_output" | tail -n 1)
-    echo "$last_line"
-    sweep_url=$(echo $last_line | sed 's/wandb: Run sweep agent with: wandb agent //g')
-
-    echo "Sweep url: $sweep_url"
 
     # Change the --job-name SBATCH argument in the job_instance.slurm file
     # to the name of the sweep config, minus .yaml
@@ -34,5 +22,5 @@ do
     sed -i "s/--job-name=.*/--job-name=$sweep_id/g" job_instance.slurm
 
     # Launch the job
-    ./job_launch.sh 0 $sweep_url
+    ./job_launch.sh 7 sqlite:///$directory/$file
 done
