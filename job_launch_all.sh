a231a889db58de3242c0afaf7f4ce3fee5905050
Zander <zkeith@mit.edu>
2023-09-28 20:23:59 -0400

    Fixed job launch all script

--- job_launch_all.sh
+++ job_launch_all.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #Launch 8 hyperparameter tuning workers for each sweep config file in the directory
-project_name="test-multi-sweep-optuna"
 
 # Activate Python environment
 source /etc/profile
 source ~/projects/disruption-survival-analysis/.venv/bin/activate
 
-directory="synthetic/test"
+device_dataset="cmod/preliminary_dataset_no_ufo"
+directory="models/$device_dataset"
 
 # Get names of all sweep config files in directory
 files=$(ls $directory | grep "sweep")
@@ -19,7 +19,7 @@ do
     # Change the --job-name SBATCH argument in the job_instance.slurm file
     # to the name of the sweep config, minus .yaml
     sweep_id=$(echo $file | sed 's/.yaml//g')
-    sed -i "s/--job-name=.*/--job-name=$sweep_id/g" job_instance.slurm
+    sed -i "s/--job-name=.*/--job-name=$sweep_id-$device_dataset/g" job_instance.slurm
 
     # Launch the job
     ./job_launch.sh 7 $directory/$file
