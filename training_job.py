afbb701b157b07ef0c52c4f97810eed1f9c484e1
Zander <zkeith@mit.edu>
2023-12-15 16:21:44 -0500

    Added memory outputs to training job

--- training_job.py
+++ training_job.py
@@ -12,23 +12,29 @@ import dill
 
 from disruption_survival_analysis.Experiments import Experiment
 from disruption_survival_analysis.experiment_utils import load_experiment_config
+from disruption_survival_analysis.manage_datasets import print_memory_usage
 
 def main(device, dataset_path, model_type, alarm_type, metric, required_warning_time_ms, working_directory=None):
 
     if working_directory is not None:
         os.chdir(working_directory)
     
+    print_memory_usage("Training before Creating Experiment")
+
     required_warning_time = float(required_warning_time_ms) / 1000
     config = load_experiment_config(device, dataset_path, model_type, alarm_type, metric, required_warning_time)
 
     # Create the experiment
     experiment = Experiment(config, 'test')
     sys.stdout.write("Experiment Trained Successfully!")
+    print_memory_usage("Training after Creating Experiment")
 
     # Cache the basic metrics
     #experiment.get_critical_metrics_vs_thresholds()
     experiment.get_critical_metrics_vs_false_alarm_rates()
     sys.stdout.write("Experiment Metrics calculated")
+
+    print_memory_usage("Training after Caching")
 
     # Make directory if it doesn't already exist
     directory_name = f"results/{device}/{dataset_path}/experiments"
@@ -38,6 +44,8 @@ def main(device, dataset_path, model_type, alarm_type, metric, required_warning_
     experiment_name = f"{model_type}_{alarm_type}_{metric}_{required_warning_time_ms}ms_experiment"
     with open(f"{directory_name}/{experiment_name}.pkl", 'wb') as f:
         dill.dump(experiment, f)
+
+    print_memory_usage("Training after Saving to File")
 
     sys.stdout.write("Experiment saved to file")
 
