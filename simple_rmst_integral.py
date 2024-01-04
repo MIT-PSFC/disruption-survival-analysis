06277a0abc1fba4ce21c9bf19d349c2690f49067
Zander <zkeith@mit.edu>
2024-01-04 15:03:37 -0500

    Added bootstrapping to rmst integral

--- simple_rmst_integral.py
+++ simple_rmst_integral.py
@@ -2,10 +2,14 @@ import os
 import sys
 
 import dill
+import numpy as np
 
 from disruption_survival_analysis.Experiments import Experiment
 from disruption_survival_analysis.manage_datasets import print_memory_usage
 from disruption_survival_analysis.experiment_utils import load_experiment_config
+from disruption_survival_analysis.critical_metrics import interquartile_mean
+
+NUM_BOOTSTRAPS = 50
 
 def main(device, dataset_path, model_type, alarm_type, metric, required_warning_time_ms, working_directory=None):
     # If an optional sixth argument is provided, change the working directory to that
@@ -24,8 +28,61 @@ def main(device, dataset_path, model_type, alarm_type, metric, required_warning_
     print_memory_usage("Simple RMST After Creating Experiment")
 
     disruptive_rmst_diffs, non_disruptive_rmst_diffs = experiment.get_simple_rmst_integrals()
+    all_diffs = np.concatenate((disruptive_rmst_diffs, non_disruptive_rmst_diffs))
+
+    results = {}
+    results['disruptive_rmst_diffs'] = disruptive_rmst_diffs
+    results['non_disruptive_rmst_diffs'] = non_disruptive_rmst_diffs
 
     print_memory_usage("Simple RMST After Getting Results")
+
+    # Bootstrap the RMST integrals individually and together
+
+    sampled_disruptive_array = np.zeros(NUM_BOOTSTRAPS)
+    sampled_non_disruptive_array = np.zeros(NUM_BOOTSTRAPS)
+    sampled_all_array = np.zeros(NUM_BOOTSTRAPS)
+
+    np.random.seed(0)
+    for i in range(NUM_BOOTSTRAPS):
+        sampled_disruptive = np.random.choice(disruptive_rmst_diffs, len(disruptive_rmst_diffs))
+        sampled_non_disruptive = np.random.choice(non_disruptive_rmst_diffs, len(non_disruptive_rmst_diffs))
+        sampled_all = np.random.choice(all_diffs, len(all_diffs))
+
+        # Calculate the mean of the sampled arrays
+        sampled_disruptive_array[i] = np.mean(sampled_disruptive)
+        sampled_non_disruptive_array[i] = np.mean(sampled_non_disruptive)
+        sampled_all_array[i] = np.mean(sampled_all)
+
+    # Report the mean, standard deviation, median, upper, and lower quartiles, and interquartile mean of the sampled arrays
+
+    disruptive_results = {}
+    disruptive_results['avg'] = np.mean(sampled_disruptive_array)
+    disruptive_results['std'] = np.std(sampled_disruptive_array)
+    disruptive_results['med'] = np.median(sampled_disruptive_array)
+    disruptive_results['iq1'] = np.percentile(sampled_disruptive_array, 25)
+    disruptive_results['iq3'] = np.percentile(sampled_disruptive_array, 75)
+    disruptive_results['iqm'] = interquartile_mean(sampled_disruptive_array)
+    results['disruptive_results'] = disruptive_results
+
+    non_disruptive_results = {}
+    non_disruptive_results['avg'] = np.mean(sampled_non_disruptive_array)
+    non_disruptive_results['std'] = np.std(sampled_non_disruptive_array)
+    non_disruptive_results['med'] = np.median(sampled_non_disruptive_array)
+    non_disruptive_results['iq1'] = np.percentile(sampled_non_disruptive_array, 25)
+    non_disruptive_results['iq3'] = np.percentile(sampled_non_disruptive_array, 75)
+    non_disruptive_results['iqm'] = interquartile_mean(sampled_non_disruptive_array)
+    results['non_disruptive_results'] = non_disruptive_results
+
+    all_results = {}
+    all_results['avg'] = np.mean(sampled_all_array)
+    all_results['std'] = np.std(sampled_all_array)
+    all_results['med'] = np.median(sampled_all_array)
+    all_results['iq1'] = np.percentile(sampled_all_array, 25)
+    all_results['iq3'] = np.percentile(sampled_all_array, 75)
+    all_results['iqm'] = interquartile_mean(sampled_all_array)
+    results['all_results'] = all_results
+
+    print_memory_usage("Simple RMST after bootstrapping")
 
     rmst_dir = f"{model_type}_{alarm_type}_{metric}_{required_warning_time_ms}ms"
 
@@ -34,12 +91,23 @@ def main(device, dataset_path, model_type, alarm_type, metric, required_warning_
     if not os.path.exists(directory_name):
         os.makedirs(directory_name)
 
-    results = {}
-    results['disruptive_rmst_diffs'] = disruptive_rmst_diffs
-    results['non_disruptive_rmst_diffs'] = non_disruptive_rmst_diffs
-
     with open(f"{directory_name}/all_rmst_results.pkl", 'wb') as f:
         dill.dump(results, f)
 
     sys.stdout.write("Saved all RMST integrals")
+
+if __name__ == "__main__":
+    # Get the device, dataset path, model, alarm, metric, min_warning_time, and working directory from the command line
+    device = sys.argv[1]
+    dataset_path = sys.argv[2]
+    model_type = sys.argv[3]
+    alarm_type = sys.argv[4]
+    metric = sys.argv[5]
+    required_warning_time_ms = sys.argv[6]
+    try:
+        working_directory = sys.argv[7]
+    except:
+        working_directory = None
+
+    main(device, dataset_path, model_type, alarm_type, metric, required_warning_time_ms, working_directory)
 
---
+++ tests/test_simple_rmst.py
@@ -0,0 +1,55 @@
+import unittest
+
+import os
+import dill
+
+from tests.test_manage_datasets import TEST_DEVICE, TEST_DATASET_PATH, TEST_WARNING_TIME
+
+from simple_rmst_integral import main
+
+class TestSimpleRMST_RF_STHR_AUROC(unittest.TestCase):
+
+    def setUp(self):
+        self.device = TEST_DEVICE
+        self.dataset_path = TEST_DATASET_PATH
+        self.model_type = 'rf'
+        self.alarm_type = 'sthr'
+        self.metric = 'auroc'
+        self.required_warning_time_ms = str(int(TEST_WARNING_TIME*1000))
+
+        self.rmst_path = f"results/{self.device}/{self.dataset_path}/simple_rmst"
+        self.category_path = f"{self.rmst_path}/{self.model_type}_{self.alarm_type}_{self.metric}_{self.required_warning_time_ms}ms"
+        self.saved_file_path = f"{self.category_path}/all_rmst_results.pkl"
+
+    def test_0_rmst_integral_completes(self):
+        """Ensure that the rmst integral job completes"""
+
+        # Remove bootstrap file if it exists
+        try:
+            os.remove(self.saved_file_path)
+        except FileNotFoundError:
+            pass
+
+        # Run the job
+        main(self.device, self.dataset_path, self.model_type, self.alarm_type, self.metric, self.required_warning_time_ms)
+
+        # Check that the bootstrap file exists
+        self.assertTrue(os.path.exists(self.saved_file_path))
+
+    def test_1_values_exist(self):
+        """Ensure the saved values are not empty"""
+
+        # Load pickle file
+        with open(self.saved_file_path, 'rb') as f:
+            results = dill.load(f)
+
+        disruptive_rmst_diffs = results['disruptive_rmst_diffs']
+        non_disruptive_rmst_diffs = results['non_disruptive_rmst_diffs']
+
+        # Check that the values are not empty
+        self.assertTrue(len(disruptive_rmst_diffs) > 0)
+        self.assertTrue(len(non_disruptive_rmst_diffs) > 0)
+
+        for result_group in ['disruptive_results', 'non_disruptive_results', 'all_results']:
+            for metric in ['avg', 'std', 'med', 'iq1', 'iq3', 'iqm']:
+                self.assertTrue(results[result_group][metric] is not None)
